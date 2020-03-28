b440861193bb49a90cf9babd76f47a572b754752
krikaz <pascal.bou@protonmail.com>
2020-03-29 00:46:35 +0100

    part 2 - PhotoCollectionViewController

--- PhotoCollection/PhotoCollection/View Controllers/PhotoCollectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/PhotoCollectionViewController.swift
@@ -87,3 +87,17 @@ class PhotoCollectionViewController: UICollectionViewController {
         }
     }
 }
+
+extension PhotoCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: 200, height: 200)
+    }
+}
